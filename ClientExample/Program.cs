27892311e486ae3fdef7b33cc355920a4cb83086
vchaplinski <107477833+vchaplinski@users.noreply.github.com>
2022-11-23 22:31:06 +0300

    Update/enhance ClientExample

--- ClientExample/Program.cs
+++ ClientExample/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Threading.Tasks;
 using Up2dateDotNet;
 
 namespace ClientExample
@@ -7,74 +8,107 @@ namespace ClientExample
     internal class Program
     {
         static IWrapper wrapper = new Wrapper();
-
+        const string prompt = "\nEnter command (s - stop, p - poll) : ";
 
         static void Main(string[] args)
         {
             Console.WriteLine("Client started");
 
-            if (args.Length < 1)
+            if (args.Length < 2)
             {
                 Console.WriteLine("Usage: ClientExample <provisioningUrl> <certificateFile>");
                 Console.WriteLine("   or: ClientExample <hawkbitUrl> <tenant> <controllerId> <token>");
             }
             else if (args.Length < 3)
             {
-                RunClientWithCertificate(args[0], args[1]);
-                Console.WriteLine("Client stopped");
+                StartClientWithCertificate(args[0], args[1]);
             }
             else
             {
-                RunClientWithDeviceToken(args[0], args[1], args[2]);
-                Console.WriteLine("Client stopped");
+                StartClientWithDeviceToken(args[0], args[1], args[2]);
+            }
+            while (true)
+            {
+                Console.Write(prompt);
+                var key = Console.ReadKey();
+                switch (key.KeyChar)
+                {
+                    case 's':
+                    case 'S':
+                        wrapper.StopClient();
+                        break;
+                    case 'p':
+                    case 'P':
+                        wrapper.RequestToPoll();
+                        break;
+                }
             }
         }
 
-        private static void RunClientWithDeviceToken(string hawkbitUrl, string controlledId, string token)
+        private static void StartClientWithDeviceToken(string hawkbitUrl, string controlledId, string token)
         {
-            wrapper.RunClientWithDeviceToken(token, hawkbitUrl + "/" + controlledId, onConfigRequest, onDeploymentAction, onCancelAction);
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    wrapper.RunClientWithDeviceToken(token, hawkbitUrl + "/" + controlledId, onConfigRequest, onDeploymentAction, onCancelAction);
+                    Console.WriteLine("\nClient restarted");
+                    Console.Write(prompt);
+                }
+            });
         }
 
-        private static void RunClientWithCertificate(string provisioningUrl, string certFile)
+        private static void StartClientWithCertificate(string provisioningUrl, string certFile)
         {
-            const string xApigToken = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
-
-            var cert = File.ReadAllText(certFile);
-
-            wrapper.RunClient(cert, provisioningUrl, xApigToken, onAuthErrorAction, onAuthSuccessAction, onConfigRequest, onDeploymentAction, onCancelAction);
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    const string xApigToken = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+                    var cert = File.ReadAllText(certFile);
+                    wrapper.RunClient(cert, provisioningUrl, xApigToken, onAuthErrorAction, onAuthSuccessAction, onConfigRequest, onDeploymentAction, onCancelAction);
+                    Console.WriteLine("\nClient restarted");
+                    Console.Write(prompt);
+                }
+            });
         }
 
         private static void onAuthErrorAction(string errorMessage)
         {
-            Console.WriteLine($"AuthErrorAction: errorMessage = {errorMessage}");
+            Console.WriteLine($"\nProvisioningErrorAction: errorMessage = {errorMessage}");
+            Console.Write(prompt);
         }
 
         private static void onAuthSuccessAction(string up2DateEndpoint)
         {
-            Console.WriteLine($"AuthSuccessAction: up2DateEndpoint = {up2DateEndpoint}");
+            Console.WriteLine($"\nProvisioningSuccessAction: up2DateEndpoint = {up2DateEndpoint}");
+            Console.Write(prompt);
         }
 
         private static bool onCancelAction(int actionId)
         {
-            Console.WriteLine($"CancelAction: actionId = {actionId}");
+            Console.WriteLine($"\nCancelAction: actionId = {actionId}");
+            Console.Write(prompt);
             return true;
         }
 
         private static void onDeploymentAction(IntPtr artifact, DeploymentInfo info, out ClientResult result)
         {
-            Console.WriteLine($"DeploymentAction: actionId = {info.id}");
+            Console.WriteLine($"\nDeploymentAction: actionId = {info.id}");
             Console.WriteLine($"    downloadType = {info.downloadType}");
             Console.WriteLine($"    updateType = {info.updateType}");
             Console.WriteLine($"    isInMaintenanceWindow = {info.isInMaintenanceWindow}");
             Console.WriteLine($"    artifactFileName = {info.artifactFileName}");
+            Console.Write(prompt);
             result = new ClientResult { Execution = Execution.CLOSED, Finished = Finished.SUCCESS, Message = string.Empty };
         }
 
         private static void onConfigRequest(IntPtr responseBuilder)
         {
-            Console.WriteLine($"ConfigRequest");
+            Console.WriteLine($"\nConfigRequest");
             wrapper.AddConfigAttribute(responseBuilder, "Example.key1", "Example.value1");
             wrapper.AddConfigAttribute(responseBuilder, "Example.key2", "Example.value2");
+            Console.Write(prompt);
         }
 
     }
